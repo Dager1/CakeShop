687e57e96002db4b3790793acf9549369c1ab943
f <franklinrohan2002@gmail.com>
2023-12-11 15:20:11 +0530

    input field component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Infofields from "./componets/Infofields";
 
 function App() {
   return (
     <div className="App">
       <div>Cake Shop</div>
+      <Infofields />
     </div>
   );
 }
