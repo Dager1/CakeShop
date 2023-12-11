cd864701de63f93935edd3c2016a77693cd907c7
f <franklinrohan2002@gmail.com>
2023-12-11 16:00:30 +0530

    adding to array and displaying array items

--- src/componets/Infofields.js
+++ src/componets/Infofields.js
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
 
 const Infofields = () => {
-  const [cupcakes, setCupcakes] = useState();
-  const [price, setPrice] = useState();
+  const [cupcakes, setCupcakes] = useState("");
+  const [price, setPrice] = useState("");
+  const [cupCakeData, setCupCakeData] = useState([]);
+
+  const handleAdd = () => {
+    const newCupCakeData = [...cupCakeData, { cupcakes, price }];
+    setCupCakeData(newCupCakeData);
+    setCupcakes("");
+    setPrice("");
+  };
+
   return (
     <div>
       <form>
-        <label>Enter the number of cup cakes</label>
-        <input
-          value={cupcakes}
-          onChange={(e) => setCupcakes(e.target.value)}></input>
-        <label>Enter the number of cup cakes</label>
-        <input value={price} onChange={(e) => setPrice(e.target.value)}></input>
+        <label>Enter the number of cupcakes:</label>
+        <input value={cupcakes} onChange={(e) => setCupcakes(e.target.value)} />
+        <label>Enter the price of cupcakes:</label>
+        <input value={price} onChange={(e) => setPrice(e.target.value)} />
+        <button type="button" onClick={handleAdd}>
+          Add
+        </button>
       </form>
-      <h2>
-        Number of cupcakes:{cupcakes} price of cupcakes:
-        {price}
-      </h2>
+      <h2>Cupcakes and Prices:</h2>
+      <ul>
+        {cupCakeData.map((item, index) => (
+          <li key={index}>
+            Number of cupcakes: {item.cupcakes} | Price of cupcakes:{" "}
+            {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
 export default Infofields;
